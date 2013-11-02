09840c9bacee4d09b4cac4cb5a0a4f2d208d8345
Rosina Germanova <gero1013 (at) hs-karlsruhe.de>
2013-11-02 10:49:06 +0100

    Klasse Artikel angelegt

---
+++ shop/src/main/java/artikelverwaltung/Artikel.java
@@ -0,0 +1,26 @@
+package artikelverwaltung;
+
+public class Artikel {
+	private long artikelNr;
+	private long preis;
+	
+	public long getArtikelNr() {
+		return artikelNr;
+	}
+	public void setArtikelNr(long artikelNr) {
+		this.artikelNr = artikelNr;
+	}
+	public long getPreis() {
+		return preis;
+	}
+	public void setPreis(long preis) {
+		this.preis = preis;
+	}
+	public Artikel(long artikelNr, long preis) {
+		super();
+		this.artikelNr = artikelNr;
+		this.preis = preis;
+	};
+	
+
+}
