41e09d476ec3f867168db1990a7cb33e3bed6afc
Lukas Gauss <Lukas Gauss@LukasGauss-PC>
2013-11-02 11:56:41 +0100

    Override ToString, Equals und GetHashCode zugefügt

--- shop/src/main/java/de/shop/kundenverwaltung/domain/Adresse.java
+++ shop/src/main/java/de/shop/kundenverwaltung/domain/Adresse.java
@@ -44,4 +44,49 @@ public class Adresse {
 	public void setOrt(String ort) {
 		this.ort = ort;
 	}
+	@Override
+	public String toString() {
+		return "Adresse [strasse=" + strasse + ", hausnummer=" + hausnummer
+				+ ", plz=" + plz + ", ort=" + ort + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((hausnummer == null) ? 0 : hausnummer.hashCode());
+		result = prime * result + ((ort == null) ? 0 : ort.hashCode());
+		result = prime * result + plz;
+		result = prime * result + ((strasse == null) ? 0 : strasse.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Adresse other = (Adresse) obj;
+		if (hausnummer == null) {
+			if (other.hausnummer != null)
+				return false;
+		} else if (!hausnummer.equals(other.hausnummer))
+			return false;
+		if (ort == null) {
+			if (other.ort != null)
+				return false;
+		} else if (!ort.equals(other.ort))
+			return false;
+		if (plz != other.plz)
+			return false;
+		if (strasse == null) {
+			if (other.strasse != null)
+				return false;
+		} else if (!strasse.equals(other.strasse))
+			return false;
+		return true;
+	}
+	
 }
