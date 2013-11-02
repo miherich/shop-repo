5780ccbb269ba1ab10d8c9d359ef6422f55070bd
Mike Herich <hemi1016 (at) hs-karlsruhe.de>
2013-11-02 12:12:14 +0100

    Object-Methoden für Klassen Kunde, Privatkunde und Geschäftskunde hinzugefügt

--- shop/src/main/java/de/shop/kundenverwaltung/domain/Privatkunde.java
+++ shop/src/main/java/de/shop/kundenverwaltung/domain/Privatkunde.java
@@ -26,6 +26,44 @@ public class Privatkunde extends Kunde {
 	public void setNachname(String nachname) {
 		this.nachname = nachname;
 	}
+
+	@Override
+	public String toString() {
+		return "Privatkunde [vorname=" + vorname + ", nachname=" + nachname
+				+ ", toString()=" + super.toString() + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result
+				+ ((nachname == null) ? 0 : nachname.hashCode());
+		result = prime * result + ((vorname == null) ? 0 : vorname.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Privatkunde other = (Privatkunde) obj;
+		if (nachname == null) {
+			if (other.nachname != null)
+				return false;
+		} else if (!nachname.equals(other.nachname))
+			return false;
+		if (vorname == null) {
+			if (other.vorname != null)
+				return false;
+		} else if (!vorname.equals(other.vorname))
+			return false;
+		return true;
+	}
 	
 	
 	
