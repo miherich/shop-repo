db56688e7fc1b2b648d5ace313dfe468df80b8da
Rosina Germanova <gero1013 (at) hs-karlsruhe.de>
2013-11-02 12:04:37 +0100

    Override Methoden in Zubehoer und Fahhrad

--- shop/src/main/java/de/shop/artikelverwaltung/domain/Fahrrad.java
+++ shop/src/main/java/de/shop/artikelverwaltung/domain/Fahrrad.java
@@ -25,6 +25,44 @@ public class Fahrrad extends Artikel {
 	public void setRahmen(String rahmen) {
 		this.rahmen = rahmen;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((bezeichnung == null) ? 0 : bezeichnung.hashCode());
+		result = prime * result + ((rahmen == null) ? 0 : rahmen.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Fahrrad other = (Fahrrad) obj;
+		if (bezeichnung == null) {
+			if (other.bezeichnung != null)
+				return false;
+		} else if (!bezeichnung.equals(other.bezeichnung))
+			return false;
+		if (rahmen == null) {
+			if (other.rahmen != null)
+				return false;
+		} else if (!rahmen.equals(other.rahmen))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Fahrrad [bezeichnung=" + bezeichnung + ", rahmen=" + rahmen
+				+ "]";
+	}
 	
 
 }
