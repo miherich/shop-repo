92f68f98440190c2de034e1f7c933a609dfe7595
Mike Herich <hemi1016 (at) hs-karlsruhe.de>
2013-11-21 17:27:36 +0100

    Import der neuen Util- und Kundenverwaltungbeispieldateien

--- shop/src/main/java/de/shop/util/Constants.java
+++ shop/src/main/java/de/shop/util/Constants.java
@@ -4,6 +4,7 @@ public final class Constants {
 	public static final String REST_PATH = "/rest";
 
 	public static final String SELF_LINK = "self";
+	public static final String LIST_LINK = "list";
 	public static final String ADD_LINK = "add";
 	public static final String UPDATE_LINK = "update";
 	public static final String REMOVE_LINK = "remove";
