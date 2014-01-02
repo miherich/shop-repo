1a39f79cc9827f5db8436a4737415d5011333fca
Mike Herich <hemi1016 (at) hs-karlsruhe.de>
2014-01-02 11:03:10 +0100

    Domainklasse Bestellung für DB Zugriff angepasst

--- shop/src/main/java/de/shop/bestellverwaltung/domain/Bestellung.java
+++ shop/src/main/java/de/shop/bestellverwaltung/domain/Bestellung.java
@@ -1,32 +1,122 @@
 package de.shop.bestellverwaltung.domain;
 
-import java.net.URI;
-import java.util.List;
+import static de.shop.util.Constants.KEINE_ID;
+import static javax.persistence.CascadeType.PERSIST;
+import static javax.persistence.CascadeType.REMOVE;
+import static javax.persistence.FetchType.EAGER;
 
+import java.lang.invoke.MethodHandles;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.Basic;
+import javax.persistence.Cacheable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedAttributeNode;
+import javax.persistence.NamedEntityGraph;
+import javax.persistence.NamedEntityGraphs;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.PostPersist;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.validation.Valid;
 import javax.validation.constraints.AssertFalse;
 import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-//import javax.xml.bind.annotation.XmlTransient;
+import javax.xml.bind.annotation.XmlTransient;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import org.jboss.logging.Logger;
 
 import de.shop.kundenverwaltung.domain.AbstractKunde;
+import de.shop.util.persistence.AbstractAuditable;
+
 
 @XmlRootElement
-public class Bestellung {
+@Entity
+@Table(indexes = {
+		@Index(columnList = "kunde_fk"),
+		@Index(columnList = "erzeugt")
+	})
+@NamedQueries({
+	@NamedQuery(name  = Bestellung.FIND_BESTELLUNGEN_BY_KUNDE, query = "SELECT b"
+				            + " FROM   Bestellung b"
+							+ " WHERE  b.kunde = :" + Bestellung.PARAM_KUNDE),
+		@NamedQuery(name  = Bestellung.FIND_KUNDE_BY_ID,
+	 			    query = "SELECT b.kunde"
+	                        + " FROM   Bestellung b"
+	  			            + " WHERE  b.id = :" + Bestellung.PARAM_ID)
+	})
+@NamedEntityGraphs({
+	@NamedEntityGraph(name = Bestellung.GRAPH_LIEFERUNGEN, attributeNodes = @NamedAttributeNode("lieferungen"))
+})
+@Cacheable
+public class Bestellung extends AbstractAuditable {
+	private static final Logger LOGGER = Logger.getLogger(MethodHandles.lookup().lookupClass());
 	
 	private static final String BESTELLUNG_BESTELLDATUM_NOTNULL_BV = "{bestellverwaltung.bestellung.bestelldatum.notNull}";
 	private static final String BESTELLUNG_ISTAUSGELIEFERT_ASSERTFALSE_BV = "{bestellverwaltung.bestellung.istAusgeliefert.assertFalse}";
 	
-	private int bestellnr;
+	private static final String PREFIX = "Bestellung.";
+	public static final String FIND_BESTELLUNGEN_BY_KUNDE = PREFIX + "findBestellungenByKunde";
+	public static final String FIND_KUNDE_BY_ID = PREFIX + "findBestellungKundeById";
+	
+	public static final String PARAM_KUNDE = "kunde";
+	public static final String PARAM_ID = "id";
+	
+	@Id
+	@GeneratedValue
+	@Basic(optional = false)
+	private int bestellnr = KEINE_ID;
+	
 	@NotNull(message = BESTELLUNG_BESTELLDATUM_NOTNULL_BV)
 	private String bestelldatum;		//TODO vernünftiges Datumsformat finden
 	@AssertFalse(message = BESTELLUNG_ISTAUSGELIEFERT_ASSERTFALSE_BV) //TODO AssertTrue?
 	private boolean istAusgeliefert;
 	private boolean mitVerpackung;
+	
+	@OneToMany(fetch = EAGER, cascade = { PERSIST, REMOVE })
+	@JoinColumn(name = "bestellung_fk", nullable = false)
+	@NotEmpty(message = "{bestellung.positionen.notEmpty}")
+	@Valid
 	private List<Position> positionen;
+	
+	@Transient
 	private URI kundeUri;
 
-//	@XmlTransient
+	@ManyToOne
+	@JoinColumn(name = "kunde_fk", nullable = false, insertable = false, updatable = false)
+	@XmlTransient
 	private AbstractKunde kunde;
+	
+	@XmlElement
+	public Date getDatum() {
+		return getErzeugt();
+	}
+	
+	public void setDatum(Date datum) {
+		setErzeugt(datum);
+	}
+	
+	@PostPersist
+	private void postPersist() {
+		LOGGER.debugf("Neue Bestellung mit ID=%d", bestellnr);
+	}
 
 	public int getBestellnr() {
 		return bestellnr;
