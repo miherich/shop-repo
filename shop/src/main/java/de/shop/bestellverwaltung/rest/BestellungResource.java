7024106995a529650b1424294f5c3b9f6bf56153
Mike Herich <hemi1016 (at) hs-karlsruhe.de>
2014-01-02 12:50:42 +0100

    Implementierung JPA BestellungResource

--- shop/src/main/java/de/shop/bestellverwaltung/rest/BestellungResource.java
+++ shop/src/main/java/de/shop/bestellverwaltung/rest/BestellungResource.java
@@ -6,10 +6,16 @@ import static javax.ws.rs.core.MediaType.APPLICATION_XML;
 import static javax.ws.rs.core.MediaType.TEXT_XML;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 //import javax.validation.Valid;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -24,7 +30,11 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 import de.shop.kundenverwaltung.rest.KundeResource;
+import de.shop.artikelverwaltung.domain.AbstractArtikel;
+import de.shop.artikelverwaltung.rest.ArtikelResource;
+import de.shop.artikelverwaltung.service.ArtikelService;
 import de.shop.bestellverwaltung.domain.Bestellung;
+import de.shop.bestellverwaltung.domain.Position;
 import de.shop.bestellverwaltung.service.BestellungService;
 //import de.shop.bestellverwaltung.domain.Position;
 import de.shop.kundenverwaltung.domain.AbstractKunde;
@@ -55,6 +65,13 @@ public class BestellungResource {
 	
 	@Inject
 	private BestellungService bs;
+	
+	@Inject
+	private ArtikelService as;
+	
+	@Inject
+	private ArtikelResource artikelResource;
+
 
 	@GET
 	public Response findAllBestellungen() {
@@ -66,7 +83,7 @@ public class BestellungResource {
 
 	@GET
 	@Path("{" + BESTELLUNG_ID_PATH_PARAM + ":[1-9][0-9]*}")
-	public Response findBestellungById(@PathParam(BESTELLUNG_ID_PATH_PARAM) int id) {
+	public Response findBestellungById(@PathParam(BESTELLUNG_ID_PATH_PARAM) Long id) {
 		// TODO Anwendungskern statt Mock, Verwendung von Locale
 		final Bestellung bestellung = bs.findBestellungById(id);
 
@@ -103,9 +120,104 @@ public class BestellungResource {
 	@POST
 	@Consumes({ APPLICATION_JSON, APPLICATION_XML, TEXT_XML })
 	@Produces
-	public Response createBestellung(Bestellung bestellung) {
+	public Response createBestellung(@Valid Bestellung bestellung) {
 		// TODO Anwendungskern statt Mock, Verwendung von Locale
-		bestellung = bs.createBestellung(bestellung);
-		return Response.created(getUriBestellung(bestellung, uriInfo)).build();
+//		bestellung = bs.createBestellung(bestellung);
+//		return Response.created(getUriBestellung(bestellung, uriInfo)).build();
+		
+		final String kundeUriStr = bestellung.getKundeUri().toString();
+		int startPos = kundeUriStr.lastIndexOf('/') + 1;
+		final String kundeIdStr = kundeUriStr.substring(startPos);
+		Long kundeId = null;
+		try {
+			kundeId = Long.valueOf(kundeIdStr);
+		}
+		catch (NumberFormatException e) {
+			kundeIdInvalid();
+		}
+		
+		// IDs der (persistenten) Artikel ermitteln
+		final Collection<Position> bestellpositionen = bestellung.getPositionen();
+		final List<Long> artikelIds = new ArrayList<>(bestellpositionen.size());
+		for (Position bp : bestellpositionen) {
+			final URI artikelUri = bp.getArtikelUri();
+			if (artikelUri == null) {
+				continue;
+			}
+			final String artikelUriStr = artikelUri.toString();
+			startPos = artikelUriStr.lastIndexOf('/') + 1;
+			final String artikelIdStr = artikelUriStr.substring(startPos);
+			Long artikelId = null;
+			try {
+				artikelId = Long.valueOf(artikelIdStr);
+			}
+			catch (NumberFormatException e) {
+				// Ungueltige Artikel-ID: wird nicht beruecksichtigt
+				continue;
+			}
+			artikelIds.add(artikelId);
+		}
+		
+		if (artikelIds.isEmpty()) {
+			// keine einzige Artikel-ID als gueltige Zahl
+			artikelIdsInvalid();
+		}
+
+		final Collection<AbstractArtikel> gefundeneArtikel = as.findArtikelByIds(artikelIds);
+		
+		// Bestellpositionen haben URLs fuer persistente Artikel.
+		// Diese persistenten Artikel wurden in einem DB-Zugriff ermittelt (s.o.)
+		// Fuer jede Bestellposition wird der Artikel passend zur Artikel-URL bzw. Artikel-ID gesetzt.
+		// Bestellpositionen mit nicht-gefundene Artikel werden eliminiert.
+		int i = 0;
+		final Set<Position> neuePositionen = new HashSet<>();
+		for (Position bp : bestellpositionen) {
+			// Artikel-ID der aktuellen Bestellposition (s.o.):
+			// artikelIds haben gleiche Reihenfolge wie bestellpositionen
+			final long artikelId = artikelIds.get(i++);
+			
+			// Wurde der Artikel beim DB-Zugriff gefunden?
+			for (AbstractArtikel artikel : gefundeneArtikel) {
+				if (artikel.getArtikelNr().longValue() == artikelId) {
+					// Der Artikel wurde gefunden
+					bp.setArtikel(artikel);
+					neuePositionen.add(bp);
+					break;					
+				}
+			}
+			// FIXME JDK 8 hat Lambda-Ausdruecke
+			//final Artikel artikel = gefundeneArtikel.stream()
+			//                                        .filter(a -> a.getId() == artikelId)
+			//                                        .findAny();
+			//if (artikel != null) {
+			//	bp.setArtikel(artikel);
+			//	neueBestellpositionen.add(bp);				
+			//}
+		}
+		bestellung.setPositionen(neuePositionen);
+		
+		bestellung = bs.createBestellung(bestellung, kundeId);
+
+		final URI bestellungUri = getUriBestellung(bestellung, uriInfo);
+		return Response.created(bestellungUri).build();
+	}
+	
+	/**
+	 * @NotNull verletzen, um die entsprechende Meldung zu verursachen, weil keine einzige Artikel-ID
+	 *          eine gueltige Zahl war.
+	 * @return null
+	 */
+	@NotNull(message = "{bestellung.artikelIds.invalid}")
+	public List<Long> artikelIdsInvalid() {
+		return null;
+	}
+	
+	/**
+	 * @NotNull verletzen, um die entsprechende Meldung zu verursachen, weil die Kunde-Id ungueltig ist.
+	 * @return null
+	 */
+	@NotNull(message = "{bestellung.kunde.id.invalid}")
+	public Long kundeIdInvalid() {
+		return null;		
 	}
 }
