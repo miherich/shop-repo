279d9437a59b1dc90194c58ded1688daa92c574f
Rosina Germanova <gero1013 (at) hs-karlsruhe.de>
2014-01-02 11:23:34 +0100

    Klasse Kunde geändert

--- shop/src/main/java/de/shop/kundenverwaltung/domain/AbstractKunde.java
+++ shop/src/main/java/de/shop/kundenverwaltung/domain/AbstractKunde.java
@@ -4,6 +4,21 @@ import java.util.ArrayList;
 import java.net.URI;
 import java.util.List;
 
+import javax.persistence.Basic;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Inheritance;
+import javax.persistence.JoinColumn;
+import javax.persistence.NamedEntityGraphs;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.OrderColumn;
+import javax.persistence.Table;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -21,6 +36,64 @@ import org.codehaus.jackson.annotate.JsonSubTypes;
 import org.codehaus.jackson.annotate.JsonSubTypes.Type;
 import org.codehaus.jackson.annotate.JsonTypeInfo;
 import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.ScriptAssert;
+import org.jboss.arquillian.drone.api.annotation.Default;
+
+@ScriptAssert(lang = "javascript",
+script = "_this.password != null && !_this.password.equals(\"\")"
+         + " && _this.password.equals(_this.passwordWdh)",
+message = "{kunde.password.notEqual}",
+groups = { Default.class, PasswordGroup.class })
+
+@Entity
+//Zu email wird unten ein UNIQUE Index definiert
+@Table (name = "kunde", indexes = @Index(columnList = "nachname"))
+@Inheritance
+@DiscriminatorColumn (name = "art", length = 1)
+@NamedQueries({
+	@NamedQuery(name  = AbstractKunde.FIND_KUNDEN,
+             query = "SELECT k"
+			        + " FROM   AbstractKunde k"),
+	@NamedQuery(name  = AbstractKunde.FIND_KUNDEN_ORDER_BY_ID,
+		        query = "SELECT   k"
+				        + " FROM  AbstractKunde k"
+		                + " ORDER BY k.id"),
+	@NamedQuery(name  = AbstractKunde.FIND_IDS_BY_PREFIX,
+		        query = "SELECT   k.id"
+		                + " FROM  AbstractKunde k"
+		                + " WHERE CONCAT('', k.id) LIKE :" + AbstractKunde.PARAM_KUNDE_ID_PREFIX
+		                + " ORDER BY k.id"),
+	@NamedQuery(name  = AbstractKunde.FIND_KUNDEN_BY_NACHNAME,
+	            query = "SELECT k"
+				        + " FROM   AbstractKunde k"
+	            		+ " WHERE  UPPER(k.nachname) = UPPER(:" + AbstractKunde.PARAM_KUNDE_NACHNAME + ")"),
+	@NamedQuery(name  = AbstractKunde.FIND_NACHNAMEN_BY_PREFIX,
+ 	            query = "SELECT   DISTINCT k.nachname"
+				        + " FROM  AbstractKunde k "
+	            		+ " WHERE UPPER(k.nachname) LIKE UPPER(:"
+	            		+ AbstractKunde.PARAM_KUNDE_NACHNAME_PREFIX + ")"),
+ 	@NamedQuery(name  = AbstractKunde.FIND_KUNDE_BY_EMAIL,
+ 	            query = "SELECT DISTINCT k"
+ 			            + " FROM   AbstractKunde k"
+ 			            + " WHERE  k.email = :" + AbstractKunde.PARAM_KUNDE_EMAIL),
+  @NamedQuery(name  = AbstractKunde.FIND_KUNDEN_BY_PLZ,
+	            query = "SELECT k"
+				        + " FROM  AbstractKunde k"
+			            + " WHERE k.adresse.plz = :" + AbstractKunde.PARAM_KUNDE_ADRESSE_PLZ),
+	@NamedQuery(name = AbstractKunde.FIND_KUNDEN_BY_DATE,
+			    query = "SELECT k"
+			            + " FROM  AbstractKunde k"
+			    		+ " WHERE k.seit = :" + AbstractKunde.PARAM_KUNDE_SEIT),
+	@NamedQuery(name = AbstractKunde.FIND_PRIVATKUNDEN_FIRMENKUNDEN,
+			    query = "SELECT k"
+			            + " FROM  AbstractKunde k"
+			    		+ " WHERE TYPE(k) IN (Privatkunde, Firmenkunde)")
+})
+@NamedEntityGraphs({
+	@NamedEntityGraph(name = AbstractKunde.GRAPH_BESTELLUNGEN,
+				  attributeNodes = @NamedAttributeNode("bestellungen")),
+	
+})
 
 @XmlRootElement
 @XmlSeeAlso({ Geschaeftskunde.class, Privatkunde.class })
@@ -46,31 +119,61 @@ public abstract class AbstractKunde implements Serializable {
 	private static final String EMAIL_PATTERN_BV = "{kundenverwaltung.kunde.email.pattern}";
 	private static final String ADRESSE_NOTNULL_BV = "{kundenverwaltung.kunde.adresse.notNull}";
 	
-	private int kundennr;
+	private static final String PREFIX = "AbstractKunde.";
+	public static final String FIND_KUNDEN = PREFIX + "findKunden";
+	public static final String FIND_KUNDEN_ORDER_BY_ID = PREFIX + "findKundenOrderById";
+	public static final String FIND_IDS_BY_PREFIX = PREFIX + "findIdsByPrefix";
+	public static final String FIND_KUNDEN_BY_NACHNAME = PREFIX + "findKundenByNachname";
+	public static final String FIND_NACHNAMEN_BY_PREFIX = PREFIX + "findNachnamenByPrefix";
+	public static final String FIND_KUNDE_BY_EMAIL = PREFIX + "findKundeByEmail";
+	public static final String FIND_KUNDEN_BY_PLZ = PREFIX + "findKundenByPlz";
+	public static final String FIND_KUNDEN_BY_DATE = PREFIX + "findKundenByDate";
+	public static final String FIND_PRIVATKUNDEN_FIRMENKUNDEN = PREFIX + "findPrivatkundenFirmenkunden";
+	
+	public static final String PARAM_KUNDE_ID = "kundeId";
+	public static final String PARAM_KUNDE_ID_PREFIX = "idPrefix";
+	public static final String PARAM_KUNDE_NACHNAME = "nachname";
+	public static final String PARAM_KUNDE_NACHNAME_PREFIX = "nachnamePrefix";
+	public static final String PARAM_KUNDE_ADRESSE_PLZ = "plz";
+	public static final String PARAM_KUNDE_SEIT = "seit";
+	public static final String PARAM_KUNDE_EMAIL = "email";
+	
+	public static final String GRAPH_BESTELLUNGEN = PREFIX + "bestellungen";
+	
+	@Id
+	@GeneratedValue
+	@Basic (optional = false)
+	private Long kundennr;
 	
 	@NotNull(message = NACHNAME_NOTNULL_BV)
 	@Size(min = NACHNAME_MIN_PATTERN, max = NACHNAME_MAX_PATTERN, message = NACHNAME_LAENGE_BV)
 	@Pattern(regexp = NACHNAME_PATTERN, message = NACHNAME_PATTERN_BV)
 	private String nachname;
 	
+	
 	@Email(message = EMAIL_PATTERN_BV)
 	@NotNull(message = EMAIL_NOTNULL_BV)
 	@Size(max = EMAIL_LENGTH_MAX, message = EMAIL_LAENGE_BV)
+	//@Column(unique = true)
 	private String email;
 	
+	@OneToOne (cascade = { PERSIST, REMOVE }, mappedBy = "kunde")
 	@NotNull(message = ADRESSE_NOTNULL_BV)
 	@Valid
 	private Adresse adresse;
 
+	@OneToMany
+	@JoinColumn (name = "kunde_fk", nullable = false)
+	@OrderColumn (name = "idx", nullable = false)
 	@XmlTransient
 	private List<Bestellung> bestellungen;
 	private URI bestellURI;
 
-	public int getKundennr() {
+	public Long getKundennr() {
 		return kundennr;
 	}
 
-	public void setKundennr(int kundennr) {
+	public void setKundennr(Long kundennr) {
 		this.kundennr = kundennr;
 	}
 
@@ -116,7 +219,7 @@ public abstract class AbstractKunde implements Serializable {
 
 	public AbstractKunde() {
 		super();
-		this.kundennr = 0;
+		this.kundennr = null;
 		this.adresse = null;
 		this.nachname = null;
 		this.bestellungen = new ArrayList<>();
@@ -129,12 +232,14 @@ public abstract class AbstractKunde implements Serializable {
 				+ ", email=" + email + "]";
 	}
 
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((email == null) ? 0 : email.hashCode());
-		result = prime * result + kundennr;
+		result = prime * result
+				+ ((kundennr == null) ? 0 : kundennr.hashCode());
 		result = prime * result
 				+ ((nachname == null) ? 0 : nachname.hashCode());
 		return result;
@@ -148,20 +253,21 @@ public abstract class AbstractKunde implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		final AbstractKunde other = (AbstractKunde) obj;
+		AbstractKunde other = (AbstractKunde) obj;
 		if (email == null) {
 			if (other.email != null)
 				return false;
-		} 
-		else if (!email.equals(other.email))
+		} else if (!email.equals(other.email))
 			return false;
-		if (kundennr != other.kundennr)
+		if (kundennr == null) {
+			if (other.kundennr != null)
+				return false;
+		} else if (!kundennr.equals(other.kundennr))
 			return false;
 		if (nachname == null) {
 			if (other.nachname != null)
 				return false;
-		} 
-		else if (!nachname.equals(other.nachname))
+		} else if (!nachname.equals(other.nachname))
 			return false;
 		return true;
 	}
