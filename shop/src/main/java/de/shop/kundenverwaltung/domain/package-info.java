c83a9211103fdc9728c260bf1c7258fd19e6e710
Stefanie Mannheim <mast1019(at)hs-karlsruhe.de>
2014-01-05 19:13:10 +0100

    @Vetoed in package-info gesetzt

--- shop/src/main/java/de/shop/kundenverwaltung/domain/package-info.java
+++ shop/src/main/java/de/shop/kundenverwaltung/domain/package-info.java
@@ -1,7 +1,9 @@
 @XmlAccessorType(FIELD)
 
+@Vetoed
 package de.shop.kundenverwaltung.domain;
 
 import static javax.xml.bind.annotation.XmlAccessType.FIELD;
 
+import javax.enterprise.inject.Vetoed;
 import javax.xml.bind.annotation.XmlAccessorType;
